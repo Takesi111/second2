335f5316c5353da0d17f1552121537687ce88289
Takesi111 <95156364+Takesi111@users.noreply.github.com>
2023-08-26 14:55:14 +0000

    hennkou

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,21 @@
 # Streamlitライブラリをインポート
 import streamlit as st
 
-# ページ設定（タブに表示されるタイトル、表示幅）
-st.set_page_config(page_title="タイトル", layout="wide")
+import geopandas as gpd
+import pandas as pd
 
-# タイトルを設定
-st.title('Streamlitのサンプルアプリ')
+# メッシュマップのシェープファイルを読み込む
+mesh_map = gpd.read_file('path_to_mesh_map_file.shp')
 
-# テキスト入力ボックスを作成し、ユーザーからの入力を受け取る
-user_input = st.text_input('あなたの名前を入力してください')
+# メッシュマップの属性データを表示して、必要な列を確認する
+print(mesh_map.head())
 
-# ボタンを作成し、クリックされたらメッセージを表示
-if st.button('挨拶する'):
-    if user_input:  # 名前が入力されているかチェック
-        st.success(f'🌟 こんにちは、{user_input}さん! 🌟')  # メッセージをハイライト
-    else:
-        st.error('名前を入力してください。')  # エラーメッセージを表示
+# 人口データを抽出する
+population_data = mesh_map[['MESH_ID', 'POPULATION_COLUMN_NAME']]
 
-# スライダーを作成し、値を選択
-number = st.slider('好きな数字（10進数）を選んでください', 0, 100)
+# 緯度経度情報を抽出する
+lat_lon_data = mesh_map[['MESH_ID', 'LATITUDE_COLUMN_NAME', 'LONGITUDE_COLUMN_NAME']]
 
-# 補足メッセージ
-st.caption("十字キー（左右）でも調整できます。")
-
-# 選択した数字を表示
-st.write(f'あなたが選んだ数字は「{number}」です。')
-
-# 選択した数値を2進数に変換
-binary_representation = bin(number)[2:]  # 'bin'関数で2進数に変換し、先頭の'0b'を取り除く
-st.info(f'🔢 10進数の「{number}」を2進数で表現すると「{binary_representation}」になります。 🔢')  # 2進数の表示をハイライト
+# 抽出したデータをCSVファイルに保存する
+population_data.to_csv('population_data.csv', index=False)
+lat_lon_data.to_csv('lat_lon_data.csv', index=False)
