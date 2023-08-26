95f8ee919cfa71bbd530683fec448ddc94d288a4
Takesi111 <95156364+Takesi111@users.noreply.github.com>
2023-08-26 15:00:14 +0000

    再変更

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,16 @@
-pip install pandas geopandas
-
 # Streamlitライブラリをインポート
 import streamlit as st
 
-import geopandas as gpd
 import pandas as pd
 
-# メッシュマップのシェープファイルを読み込む
-mesh_map = gpd.read_file('path_to_mesh_map_file.shp')
-
-# メッシュマップの属性データを表示して、必要な列を確認する
-print(mesh_map.head())
+# メッシュマップデータのCSVファイルを読み込む（仮のデータ形式）
+mesh_map_data = pd.read_csv('mesh_map_data.csv')
 
 # 人口データを抽出する
-population_data = mesh_map[['MESH_ID', 'POPULATION_COLUMN_NAME']]
+population_data = mesh_map_data[['MESH_ID', 'POPULATION_COLUMN']]
 
-# 緯度経度情報を抽出する
-lat_lon_data = mesh_map[['MESH_ID', 'LATITUDE_COLUMN_NAME', 'LONGITUDE_COLUMN_NAME']]
+# 緯度経度データを抽出する
+lat_lon_data = mesh_map_data[['MESH_ID', 'LATITUDE_COLUMN', 'LONGITUDE_COLUMN']]
 
 # 抽出したデータをCSVファイルに保存する
 population_data.to_csv('population_data.csv', index=False)
