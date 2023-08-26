1253fa5f4fb67a7b482bcb934b810d1558bd350f
Takesi111 <95156364+Takesi111@users.noreply.github.com>
2023-08-26 16:13:14 +0000

    いい

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,38 @@
-# Streamlitライブラリをインポート
 import streamlit as st
+import cv2
+import numpy as np
+import csv
 
-import streamlit as st
-import pandas as pd
+def main():
+    st.title("人口メッシュマップデータ抽出アプリ")
 
-# ファイルアップロード部分
-st.title('メッシュマップからデータを抽出するアプリ')
-file = st.file_uploader("メッシュマップデータのCSVファイルをアップロードしてください", type=["csv"])
+    uploaded_file = st.file_uploader("人口メッシュマップ画像をアップロード", type=["png", "jpg", "jpeg"])
 
-if file is not None:
-    mesh_map_data = pd.read_csv(file)
+    if uploaded_file is not None:
+        st.image(uploaded_file, caption="アップロードされた画像", use_column_width=True)
+        st.write("画像を処理してデータを抽出中...")
 
-    st.subheader("抽出したいデータを選択してください")
-    selected_data = st.multiselect("データを選択", mesh_map_data.columns)
+        # 画像データの読み込み
+        image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), 1)
 
-    if selected_data:
-        selected_data_df = mesh_map_data[selected_data]
-        st.dataframe(selected_data_df)
+        # 画像処理およびデータ抽出処理を実装
+        # 以下に適切な画像処理コードを挿入
 
-        # 人口データ抽出
-        if '人口データ' in selected_data:
-            population_data = selected_data_df[['MESH_ID', '人口データ']]
-            st.write("人口データ:")
-            st.dataframe(population_data)
+        # 座標と人口データのリストを想定
+        coordinates = [(latitude1, longitude1, population1),
+                       (latitude2, longitude2, population2),
+                       # ... 他の座標と人口データ ...
+                      ]
 
-        # 緯度経度データ抽出
-        if '緯度データ' in selected_data and '経度データ' in selected_data:
-            lat_lon_data = selected_data_df[['MESH_ID', '緯度データ', '経度データ']]
-            st.write("緯度経度データ:")
-            st.dataframe(lat_lon_data)
+        # CSV ファイルにデータを書き込む
+        csv_file = "extracted_data.csv"
+        with open(csv_file, "w", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow(["Latitude", "Longitude", "Population"])  # ヘッダー行
+            for lat, lon, pop in coordinates:
+                writer.writerow([lat, lon, pop])
+
+        st.write("データを抽出し、ファイルに保存しました。")
+
+if __name__ == "__main__":
+    main()
